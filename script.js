ca0f53737474894ba64e7836076add0a6cbe4be0
Jayesh Sharma <jayeshsh1707@gmail.com>
2023-06-10 19:56:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 //your code here
+let table = document.querySelector("table");
+let priceList = document.querySelectorAll("[data-ns-test=price]");
+// console.log(priceList)
+let sum = 0;
+for(let i=0; i<priceList.length; i++){
+    sum += parseInt(priceList[i].innerText);
+}
+// console.log(sum)
+
+let lastRow = document.createElement("tr");
+let tableData = document.createElement("td");
+tableData.setAttribute("data-ns-test", "grandTotal");
+tableData.innerText = sum;
+lastRow.appendChild(tableData);
+table.appendChild(lastRow);
